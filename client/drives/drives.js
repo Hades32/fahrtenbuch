ba83c60c1783120061b4d6149bbb3009279b2ad3
Martin Rauscher <hades32@gmail.com>
2014-05-31 13:46:59 +0200

    refactored code structure and added drive distance

--- client/drives/drives.js
+++ client/drives/drives.js
@@ -1,7 +1,4 @@
 
-// ----------------- Drives ----------------------
-
-
 Template.drives.any_vehicle_selected = function () {
     return ! Session.equals('selected_vehicle_plate', null);
 };
@@ -33,64 +30,10 @@ Template.driveListItem.endTime = function(){
     return new Date(this.end).toLocaleString();
 };
 
-Template.newDrive.events({
-    'click .save':
-        function (event, template) {
-            var curVehicle = Session.get('selected_vehicle_plate');
-            var description = template.find(".description").value;
-            var privateDrive = template.find(".private").checked;
-            var start = template.find(".start").value;
-            var end = template.find(".end").value;
-            var successfulRentals = template.find(".successfulRentals").value || 0;
-            var failedRentals = template.find(".failedRentals").value || 0;
-            var startKM = template.find(".startKM").value;
-            var endKM = template.find(".endKM").value;
-            var selectedTester = template.find(".selectedTester").value;
+Template.driveListItem.distance = function(){
+    return this.endKM - this.startKM;
+};
 
-            if (end < start){
-                alert("Ende liegt vor Start!");
-                return;
-            }
-
-            var newDrive =
-            {
-                description: description,
-                vehicle: curVehicle,
-                privateDrive: privateDrive,
-                start : start,
-                end : end,
-                successfulRentals : successfulRentals,
-                failedRentals : failedRentals,
-                testerId : selectedTester,
-                startKM : startKM,
-                endKM : endKM
-            };
-            var id = Drives.insert(newDrive);
-
-            if (id){
-                template.find(".description").value = "";
-                template.find(".start").value = "";
-                template.find(".end").value = "";
-                template.find(".successfulRentals").value  = "";
-                template.find(".failedRentals").value  = "";
-                template.find(".startKM").value = "";
-                template.find(".endKM").value = "";
-            }
-        },
-
-    'click .cancel':
-        function () {
-            //mode clean somethings
-        }
-});
-
-Template.newDrive.events({
-    'click .newTester':
-        function (event, template) {
-            Session.set('new_tester_open', true);
-        }
-});
-
-Template.newDrive.testerList = function(){
-    return Testers.find({},{sort:{name:1}});
+Template.driveListItem.distanceIssue = function(){
+    return (this.endKM - this.startKM) > 120 ? "issue" : "";
 };
---
+++ client/drives/newDrive.js
@@ -0,0 +1,76 @@
+
+Template.newDrive.events({
+    'click .save':
+        function (event, template) {
+            var curVehicle = Session.get('selected_vehicle_plate');
+            var description = template.find(".description").value;
+            var privateDrive = template.find(".private").checked;
+            var start = template.find(".start").value;
+            var end = template.find(".end").value;
+            var successfulRentals = template.find(".successfulRentals").value || 0;
+            var failedRentals = template.find(".failedRentals").value || 0;
+            var startKM = template.find(".startKM").value;
+            var endKM = template.find(".endKM").value;
+            var selectedTester = template.find(".selectedTester").value;
+
+            if (end < start){
+                alert("Zeit: Ende liegt vor Start!");
+                return;
+            }
+
+            if (endKM < startKM){
+                alert("KM: Ende liegt vor Start!");
+                return;
+            }
+
+            var newDrive =
+            {
+                description: description,
+                vehicle: curVehicle,
+                privateDrive: privateDrive,
+                start : start,
+                end : end,
+                successfulRentals : successfulRentals,
+                failedRentals : failedRentals,
+                testerId : selectedTester,
+                startKM : startKM,
+                endKM : endKM
+            };
+            var id = Drives.insert(newDrive);
+
+            if (id){
+                template.find(".description").value = "";
+                template.find(".start").value = "";
+                template.find(".end").value = "";
+                template.find(".successfulRentals").value  = "";
+                template.find(".failedRentals").value  = "";
+                template.find(".startKM").value = "";
+                template.find(".endKM").value = "";
+            }
+        },
+
+    'click .cancel':
+        function () {
+            //mode clean somethings
+        }
+});
+
+Template.newDrive.events({
+    'click .newTester':
+        function (event, template) {
+            Session.set('new_tester_open', true);
+        }
+});
+
+Template.newDrive.testerList = function(){
+    return Testers.find({},{sort:{name:1}});
+};
+
+Template.newDrive.lastEndKm = function(){
+    var curVehicle = Session.get('selected_vehicle_plate');
+    var lastDrive = Drives.findOne({vehicle: curVehicle}, {sort: {end: -1}});
+    if (lastDrive)
+        return lastDrive.endKM;
+    else
+        return 0;
+};
