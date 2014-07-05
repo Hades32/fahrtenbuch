577ad3420d825cba9481c23bbc04edf604dfe872
Martin Rauscher <hades32@gmail.com>
2014-07-05 20:58:43 +0200

    add method to get drives in a given month

--- collections/collections.js
+++ collections/collections.js
@@ -5,3 +5,31 @@ Vehicles = new Meteor.Collection("vehicles");
 Drives = new Meteor.Collection("drives");
 //TODO describe
 Testers = new Meteor.Collection("testers");
+
+
+
+Drives.findInMonthOfDay = function (dayInMonth) {
+    var thisYear = dayInMonth.getFullYear();
+    var thisMonth = dayInMonth.getMonth();
+    var startOfCurrentMonth = new Date(thisYear, thisMonth, 1, 0, 0, 0);
+    var startOfNextMonth = new Date(thisYear, thisMonth + 1, 1, 0, 0, 0);
+    var endOfCurrentMonth = new Date(startOfNextMonth.getTime() - 1);
+
+    return Drives.find(
+        {
+            privateDrive: true,
+            $or: [
+                { $and: [
+                    {start: { $gt: startOfCurrentMonth } },
+                    { end: { $lt: endOfCurrentMonth } }
+                ] },
+                { $and: [
+                    {start: { $lt: startOfCurrentMonth } },
+                    { end: { $gt: startOfCurrentMonth } }
+                ] },
+                { $and: [
+                    {start: { $lt: endOfCurrentMonth } },
+                    { end: { $gt: endOfCurrentMonth } }
+                ] }
+            ]});
+};
